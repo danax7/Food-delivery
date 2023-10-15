09a2062663d22ba5390f3e48109e20780da203d8
Daniil Khakhulin <dan.praeco@gmail.com>
2023-10-15 12:40:49 +0700

    Add menu list fetching using redux toolkit

---
+++ src/modules/MenuList/Model/slice.ts
@@ -0,0 +1,44 @@
+// src/features/menu/menuSlice.ts
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { initialState } from "./state";
+import { MenuState } from "./types";
+
+interface FetchMenuParams {
+  categories: string[];
+  vegetarian: boolean;
+  page: number;
+}
+
+export const fetchMenu = createAsyncThunk<MenuState["dishes"], FetchMenuParams>(
+  "menu/fetchMenu",
+  async (params) => {
+    const response = await axios.get(
+      "https://food-delivery.kreosoft.ru/api/dish",
+      { params }
+    );
+    return response.data.dishes;
+  }
+);
+
+const menuSlice = createSlice({
+  name: "menu",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMenu.pending, (state) => {
+        state.loading = "pending";
+      })
+      .addCase(fetchMenu.fulfilled, (state, action) => {
+        state.loading = "succeeded";
+        state.dishes = action.payload;
+      })
+      .addCase(fetchMenu.rejected, (state, action) => {
+        state.loading = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default menuSlice.reducer;
---
+++ src/modules/MenuList/Model/types.ts
@@ -0,0 +1,16 @@
+export interface Dish {
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  vegetarian: boolean;
+  rating: number;
+  category: string;
+  id: string;
+}
+
+export interface MenuState {
+  dishes: Dish[];
+  loading: "idle" | "pending" | "succeeded" | "failed";
+  error: string | null;
+}
